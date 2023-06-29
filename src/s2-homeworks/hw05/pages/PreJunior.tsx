acbc939646f1e03830f76d817fc3304198ea0c30
h12t20 <sergdiamond@ymail.com>
2023-06-29 17:32:15 +0300

    sort

--- src/s2-homeworks/hw05/pages/PreJunior.tsx
+++ src/s2-homeworks/hw05/pages/PreJunior.tsx
@@ -8,7 +8,7 @@ function PreJunior() {
     return (
         <div id={'hw5-page-pre-junior'}>
             {/*pre junior page*/}
-            <HW1 />
+            {/*<HW1 />*/}
            {/* <HW2 />
             <HW3 />
             <HW4 />*/}
--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -41,18 +41,21 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
     spanProps || {}
 
     const onEnterCallback = () => {
+        setEditMode(!editMode)
         // выключить editMode при нажатии Enter // делают студенты
 
         onEnter?.()
+
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
+        setEditMode(!editMode)
         // выключить editMode при нажатии за пределами инпута // делают студенты
 
         onBlur?.(e)
     }
     const onDoubleClickCallBack = (
         e: React.MouseEvent<HTMLSpanElement, MouseEvent>
-    ) => {
+    ) => { setEditMode(!editMode)
         // включить editMode при двойном клике // делают студенты
 
         onDoubleClick?.(e)
