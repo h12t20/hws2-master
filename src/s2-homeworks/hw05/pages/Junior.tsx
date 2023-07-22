bebea46eef15a7423680e51b61615f7b8c1e0f84
h12t20 <sergdiamond@ymail.com>
2023-07-22 11:25:31 +0300

    hw6

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -7,9 +7,9 @@ import HW8 from '../../hw08/HW8'
 function Junior() {
     return (
         <div id={'hw5-page-junior'}>
-            {/*<HW6 />*/}
+            <HW6 />
            {/* <HW7 />*/}
-            <HW8 />
+           {/* <HW8 />*/}
             {/*<HW9 />*/}
         </div>
     )
