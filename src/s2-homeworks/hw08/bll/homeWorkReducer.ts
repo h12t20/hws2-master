f33ca647572ea71205d2e80c86d849026308c045
h12t20 <sergdiamond@ymail.com>
2023-08-04 22:05:59 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -13,9 +13,9 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            const compareSortFn = (a: UserType, b: UserType) => a.name < b.name ? 1 : -1
+            const compareSortFn = (a: UserType, b: UserType) => a.name < b.name ? -1 : 1
             return action.payload === 'down' ? [...state].sort((a: UserType, b: UserType) =>
-                a.name < b.name ? -1 : 1) : action.payload === 'up' ?
+                a.name < b.name ? 1 : -1) : action.payload === 'up' ?
                 [...state].sort(compareSortFn) : state // need to fix
         }
         case 'check': {
